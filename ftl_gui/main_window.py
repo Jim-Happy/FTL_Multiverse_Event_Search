1570e24b2487c0d03418045cc2420b2d7fdc3b26
Jim <zengzhy8@e.gzhu.edu.cn>
2026-05-31 13:23:09 +0800

    feat:添加图标和关于

--- ftl_gui/main_window.py
+++ ftl_gui/main_window.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 
 import sys
+from pathlib import Path
 
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtGui import QAction, QIcon
+from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox
 
 from ftl_dao import FTLDAO
 
@@ -38,13 +40,49 @@ class FTLSearchMainWindow(
         super().__init__()
         self.setWindowTitle("FTL 事件查询工具")
         self.resize(1280, 800)
+        self._set_app_icon()
         self.dao = FTLDAO()
+        self._build_menu_bar()
         self._build_ui()
         self._install_tree_rendering()
         self._load_demo_content()
         self.search_button.clicked.connect(self.on_search)
         self.search_input.returnPressed.connect(self.on_search)
         self.event_list.currentItemChanged.connect(self.on_event_list_current_item_changed)
+
+    def _icon_path(self) -> Path:
+        frozen_root = getattr(sys, "_MEIPASS", None)
+        if frozen_root is not None:
+            return Path(frozen_root) / "icon" / "icon.ico"
+        return Path(__file__).resolve().parents[1] / "icon" / "icon.ico"
+
+    def _set_app_icon(self) -> None:
+        icon = QIcon(str(self._icon_path()))
+        self.setWindowIcon(icon)
+        app = QApplication.instance()
+        if app is not None:
+            app.setWindowIcon(icon)
+
+    def _build_menu_bar(self) -> None:
+        help_menu = self.menuBar().addMenu("帮助")
+        about_action = QAction("关于", self)
+        about_action.triggered.connect(self._show_about_dialog)
+        help_menu.addAction(about_action)
+
+    def _show_about_dialog(self) -> None:
+        html = (
+            '<h2>FTL 事件查询器 (Multiverse)</h2>'
+            '<p><b>版本：</b> v1.0.0</p>'
+            '<p><b>作者：</b> 皓月如心</p>'
+            '<p><b>github：</b> <a href="https://github.com/Jim-Happy/FTL_Multiverse_Event_Search">'
+            'https://github.com/Jim-Happy/FTL_Multiverse_Event_Search</a></p>'
+            '<hr>'
+            '<p><b>说明：</b></p>'
+            '<p>本工具专为 FTL Multiverse 模组开发。<br>'
+            '支持本地化数据解析、事件树追踪及底层奖励透视。</p>'
+            '<p><b>特别鸣谢：</b> FTL 汉化组，SlipstreamModManager开发者及 Mod 开发者们</p>'
+        )
+        QMessageBox.about(self, "关于", html)
 
     def closeEvent(self, event) -> None:  # type: ignore[override]
         try:
